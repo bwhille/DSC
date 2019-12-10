c5383aea24e7fa131141966c310e4e92395f4801
FFNemmenich <FFNemmenich@users.noreply.github.com>
2019-12-10 16:23:29 +0100

    Create DSC_Firewall.ps1
    DSC_Firewall

---
+++ DSC_Firewall.ps1
@@ -0,0 +1,68 @@
+﻿Configuration FirewallProfile_ConfigurePrivateFirewallProfile_Config
+{
+if (Get-Module -ListAvailable -Name NetworkingDsc) {
+    Write-Host "Module exists"
+    }
+else {
+      Write-Host "Module does not exist"
+      Find-Module NetworkingDsc | Install-Module
+    }
+    Import-DscResource -Module NetworkingDsc
+
+    Node localhost
+    {
+        FirewallProfile ConfigurePrivateFirewallProfile
+        {
+            Name = 'Private'
+            Enabled = 'False'
+#            DefaultInboundAction = 'Block'
+#            DefaultOutboundAction = 'Allow'
+#            AllowInboundRules = 'True'
+#            AllowLocalFirewallRules = 'False'
+#            AllowLocalIPsecRules = 'False'
+#            NotifyOnListen = 'True'
+#            LogFileName = '%systemroot%\system32\LogFiles\Firewall\pfirewall.log'
+#            LogMaxSizeKilobytes = 16384
+#            LogAllowed = 'False'
+#            LogBlocked = 'True'
+#            LogIgnored = 'NotConfigured'
+        }
+                FirewallProfile ConfigureDomainFirewallProfile
+        {
+            Name = 'Domain'
+            Enabled = 'False'
+#            DefaultInboundAction = 'Block'
+#            DefaultOutboundAction = 'Allow'
+#            AllowInboundRules = 'True'
+#            AllowLocalFirewallRules = 'False'
+#            AllowLocalIPsecRules = 'False'
+#            NotifyOnListen = 'True'
+#            LogFileName = '%systemroot%\system32\LogFiles\Firewall\pfirewall.log'
+#            LogMaxSizeKilobytes = 16384
+#            LogAllowed = 'False'
+#            LogBlocked = 'True'
+#            LogIgnored = 'NotConfigured'
+        }
+                FirewallProfile ConfigurePublicFirewallProfile
+        {
+            Name = 'Public'
+            Enabled = 'False'
+#            DefaultInboundAction = 'Block'
+#            DefaultOutboundAction = 'Allow'
+#            AllowInboundRules = 'True'
+#            AllowLocalFirewallRules = 'False'
+#            AllowLocalIPsecRules = 'False'
+#            NotifyOnListen = 'True'
+#            LogFileName = '%systemroot%\system32\LogFiles\Firewall\pfirewall.log'
+#            LogMaxSizeKilobytes = 16384
+#            LogAllowed = 'False'
+#            LogBlocked = 'True'
+#            LogIgnored = 'NotConfigured'
+        }
+
+
+    }
+}
+FirewallProfile_ConfigurePrivateFirewallProfile_Config -OutputPath c:\dsc_script
+
+Start-DscConfiguration -Path C:\dsc_script -Verbose -Wait -Force
