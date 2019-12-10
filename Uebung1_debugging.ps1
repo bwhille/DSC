4e780ca46a42d94db4b27945f655caea14a3a747
bwhille <hille@online.de>
2019-12-10 09:16:29 +0100

    Test

---
+++ Uebung1_debugging.ps1
@@ -0,0 +1,19 @@
+﻿ function get-timeup {
+Param ( [String] $RechnerName = $env:COMPUTERNAME )
+$daten = Get-WmiObject Win32_OperatingSystem -ComputerName $RechnerName
+if($daten.LastBootUpTime)
+    {
+    $startzeit = $daten.ConvertToDateTime($daten.LastBootUpTime)
+    $zeitspanne = (Get-Date) - $startzeit
+
+    Write-Output ("Rechnername : $RechnerName")
+    Write-Output ("Letzter Start: " + ($startzeit).ToString("dd.MM.yyyy hh:mn:ss"))
+    Write-Output ("Läuft seit  : " + $zeitspanne.Days + "Tage " + $zeitspanne.Hours + " Stunden " + $zeitspanne.Minutes + " Minuten")
+    }
+else
+    {
+    Write-Warning "Keine Verbindung zu $RechnerName."
+    }
+} 
+
+get-timeup
